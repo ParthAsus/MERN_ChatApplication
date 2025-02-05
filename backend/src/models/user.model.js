617a74063910fb74a0a4508308df2789340aab9e
Shivam Chopra <parthasus7973@gmail.com>
2025-02-05 20:28:13 +0530

    Converted backend from submodule to regular folder

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,31 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    email:{
+      type:String,
+      required:true,
+      unique:true
+    },
+    fullName:{
+      type:String,
+      required:true
+    },
+    password:{
+      type:String,
+      required:true,
+      minlength: 6,
+    },
+    profilePic:{
+      type:String,
+      default: '',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const user = mongoose.model('User', UserSchema);
+export default user;  
