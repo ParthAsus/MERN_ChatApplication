d6bff534735fdae9616f523e7e9ea146898e3823
Shivam Chopra <parthasus7973@gmail.com>
2025-02-23 21:04:26 +0530

    trying deploying again

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,9 +28,13 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/groups", groupRoutes);
 
 if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+  })
+}else{
+  app.get("/", (req, res) => {
+    res.send("Api is running"); 
   })
 }
 // ..
